77dc5e2c30a9f9c1afdb21508af8a769a3008731
implicitlycorrect <121817193+implicitlycorrect@users.noreply.github.com>
2024-01-07 18:34:21 +0100

    Initial commit

---
+++ Serilog.Sinks.DiscordWebhook/DiscordWebhookSink.cs
@@ -0,0 +1,15 @@
+﻿using JNogueira.Discord.Webhook.Client;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace Serilog.Sinks.DiscordWebhook;
+
+public sealed class DiscordWebhookSink(string webhookUrl) : ILogEventSink
+{
+    private readonly DiscordWebhookClient _webhookClient = new(webhookUrl);
+
+    public void Emit(LogEvent logEvent)
+    {
+        _webhookClient.SendToDiscord(new DiscordMessage(logEvent.RenderMessage()), true);
+    }
+}
---
+++ Serilog.Sinks.DiscordWebhook/Extensions.cs
@@ -0,0 +1,11 @@
+﻿using Serilog.Configuration;
+
+namespace Serilog.Sinks.DiscordWebhook;
+
+public static class Extensions
+{
+    public static LoggerConfiguration DiscordWebhook(this LoggerSinkConfiguration sinkConfiguration, string webhookUrl)
+    {
+        return sinkConfiguration.Sink(new DiscordWebhookSink(webhookUrl));
+    }
+}
