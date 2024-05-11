fe3bf0a89221fd191b5d745a473545c3c54b9d65
implicitlycorrect <121817193+implicitlycorrect@users.noreply.github.com>
2024-05-11 14:05:52 +0200

    Update

--- Serilog.Sinks.DiscordWebhook/DiscordWebhookSink.cs
+++ Serilog.Sinks.DiscordWebhook/DiscordWebhookSink.cs
@@ -1,15 +1,30 @@
 ﻿using JNogueira.Discord.Webhook.Client;
 using Serilog.Core;
 using Serilog.Events;
+using Serilog.Formatting;
 
 namespace Serilog.Sinks.DiscordWebhook;
 
-public sealed class DiscordWebhookSink(string webhookUrl) : ILogEventSink
+public sealed class DiscordWebhookSink(string webhookUrl, ITextFormatter? textFormatter = null) : ILogEventSink
 {
     private readonly DiscordWebhookClient _webhookClient = new(webhookUrl);
 
     public void Emit(LogEvent logEvent)
     {
-        _webhookClient.SendToDiscord(new DiscordMessage(logEvent.RenderMessage()), true);
+        try
+        {
+            using var writer = new StringWriter();
+            if (textFormatter is not null)
+                textFormatter.Format(logEvent, writer);
+            else
+                writer.WriteLine(logEvent.RenderMessage());
+            
+            var logMessage = writer.ToString();
+            _webhookClient.SendToDiscord(new DiscordMessage(logMessage), true);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
     }
 }
--- Serilog.Sinks.DiscordWebhook/Extensions.cs
+++ Serilog.Sinks.DiscordWebhook/Extensions.cs
@@ -1,11 +1,12 @@
 ﻿using Serilog.Configuration;
+using Serilog.Formatting;
 
 namespace Serilog.Sinks.DiscordWebhook;
 
 public static class Extensions
 {
-    public static LoggerConfiguration DiscordWebhook(this LoggerSinkConfiguration sinkConfiguration, string webhookUrl)
+    public static LoggerConfiguration DiscordWebhook(this LoggerSinkConfiguration sinkConfiguration, string webhookUrl, ITextFormatter? textFormatter = null)
     {
-        return sinkConfiguration.Sink(new DiscordWebhookSink(webhookUrl));
+        return sinkConfiguration.Sink(new DiscordWebhookSink(webhookUrl, textFormatter));
     }
 }
